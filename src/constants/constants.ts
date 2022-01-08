0adf5980e90b681175d18f0d968c51b774d193aa
Unekwe Francis <unekwenchekwube@gmail.com>
2022-01-08 18:44:50 +0100

    added constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,11 @@
-export interface bookType{
-    name: string;
-    genre:string;
-    author:string;
-}
+import { tabProps } from "./types";
+import bookTab from "../assets/booktab.svg";
+import homeTab from "../assets/hometab.svg";
+import writerTab from "../assets/writertab.svg";
+import searchTab from "../assets/search.svg";
+export const tabItems: tabProps[] = [
+    { image: homeTab, text: "home" },
+    { image: bookTab, text: "books" },
+    { image: writerTab, text: "authors" },
+    { image: searchTab, text: "search" },
+  ];
