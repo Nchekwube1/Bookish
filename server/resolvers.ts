8e993f78614f1991eb5fbb74d438146c5875268b
Unekwe Francis <unekwenchekwube@gmail.com>
2022-01-08 18:12:21 +0100

    added resolversfor book, author query and mutation

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -1,32 +1,31 @@
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const authors = [
-    {
-        name:"chimamanda adichie"
-    },
-    {
-        name:"dan brown"
-    },
-    {
-        name:"ola rotimi"
-    },
-]
-
+import { getRepository } from "typeorm";
+import { Book } from "./entity/Book";
+import { Author } from "./entity/Author";
+import {bookType} from './constants/types'
 const resolvers = {
   Query: {
-    books: () => books,
-    authors: () => authors,
+    books: async () => {
+      const books = await getRepository(Book).find()
+      return books
+    },
+    authors: async () => {
+      const authors = getRepository(Author).find()
+    },
 
-  }
+  },
+  Mutation: {
+    addBook: async(_:any, {title, author, genre}: bookType) => {
+      console.log(title, author, genre)
+      const book = new Book();
+      book.author = author;
+      book.genre = genre;
+      book.title = title;
+      
+      const insetedData = await getRepository(Book).save(book)
+
+      return insetedData
+    }
+  },
 };
 
 export default resolvers
