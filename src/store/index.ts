8f11074ffa52b344d0ea50c258b85d289c2ac4a7
Unekwe Francis <unekwenchekwube@gmail.com>
2022-01-08 18:45:12 +0100

    added homeslice to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux'
 import sideSlice from "../components/sidebar/sideSlice"
+import homeSlice from '../components/home/homeSlice'
 const store = configureStore({
   reducer: {
-    sideSlice
+    sideSlice,
+    homeSlice
   },
 })
 export type RootState = ReturnType<typeof store.getState>
